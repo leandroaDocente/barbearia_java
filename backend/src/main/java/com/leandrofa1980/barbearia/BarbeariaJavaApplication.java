e622266995e95c7894860e346bbddbae4613409c
leandrofa1980 <leandrofa1980@gmail.com>
2025-11-12 10:17:41 -0300

    projeto iniciado

---
+++ backend/src/main/java/com/leandrofa1980/barbearia/BarbeariaJavaApplication.java
@@ -0,0 +1,13 @@
+package com.leandrofa1980.barbearia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BarbeariaJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BarbeariaJavaApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/leandrofa1980/barbearia/BarbeariaJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leandrofa1980.barbearia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BarbeariaJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
